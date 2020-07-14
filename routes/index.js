dbfb0f381b3f22e5d12f35890c1b01fd409b183e
Зубков Валерий Павлович <v.zubkov@bssys.com>
2020-07-14 22:15:40 +0400

    init

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+var sqlite3 = require('sqlite3').verbose()
+var db =  new sqlite3.Database(path.resolve(__dirname, '../db2.db'), (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the chinook database.');
+});
+
+db.serialize(function (err) {
+  db.all('SELECT * FROM users', function (err, row) {
+    console.log(row, err);
+    // console.log(row.id + ': ' + row.name)
+  })
+})
+
+
+
+console.log(db);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
