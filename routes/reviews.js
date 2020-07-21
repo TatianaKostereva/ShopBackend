143123a8a84c26a0288bc39b7926f6c201678967
Зубков Валерий Павлович <v.zubkov@bssys.com>
2020-07-21 23:00:54 +0400

    init

--- routes/reviews.js
+++ routes/reviews.js
@@ -11,12 +11,22 @@ const db =  new sqlite3.Database(path.resolve(__dirname, '../db.db'), (err) => {
   console.log('Connected to the chinook database.');
 });
 
-/* GET products listing. */
-router.get('/', async function(req, res, next) {
-  const dbAll = promiseDecorator(db.all.bind(db));
-  const reviews = await dbAll('SELECT * FROM reviews');
 
-  res.json(reviews);
-});
+async function init() {
+  router.get('/get_by_product/:id', async function(req, res, next) {
+    const dbGet = promiseDecorator(db.all.bind(db));
+    const id = req.params.id;
+    const reviews = await dbGet(`SELECT * FROM reviews WHERE product_id=${id}`);
+
+    console.log(reviews)
+    res.json(reviews);
+  });
+}
+
+init().then(() => {
+  console.log('reviews init');
+})
+/* GET products listing. */
+
 
 module.exports = router;
