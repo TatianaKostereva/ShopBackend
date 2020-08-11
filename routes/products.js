959196e8c7cd295467b0cb9a33dea8ad1ce520fb
Зубков Валерий Павлович <v.zubkov@bssys.com>
2020-08-11 22:56:51 +0400

    init

--- routes/products.js
+++ routes/products.js
@@ -15,15 +15,27 @@ router.get('/', async function(req, res, next) {
 
 /* GET products listing from start to end */
 async function choice() {
+  const dbGet = promiseDecorator(db.all.bind(db));
+
+  router.get('/load_by_ids/', async function(req, res, next) {
+    console.log(req);
+    const products = await dbGet(`SELECT * FROM products WHERE id in (${req.query.ids.join(',')})`);
+
+    res.json([]);
+  });
+
   router.get('/:start/:end', async function(req, res, next) {
-    const dbGet = promiseDecorator(db.all.bind(db));
     const start = req.params.start;
     const end = req.params.end;
     const products = await dbGet(`SELECT * FROM products WHERE id BETWEEN ${start} AND ${end}`);
 
     res.json(products);
   });
+
+
 }
+
+
 
 choice().then(() => {
   console.log('products choice');
