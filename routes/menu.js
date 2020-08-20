510b9326dc2bc3357b8bd7c50fc839fefc96e6bf
TatianaKostereva <tatjana.kostereva@yandex.ru>
2020-08-20 21:04:50 +0300

    fix menu, fix reviews

--- routes/menu.js
+++ routes/menu.js
@@ -8,9 +8,7 @@ const db = getDB();
 /* GET menu listing. */
 router.get('/', async function(req, res, next) {
   const dbAll = promiseDecorator(db.all.bind(db));
-  const menu = await dbAll('select * from menu join menu_children using(menu_id)');
-  const children = await dbAll('select * from children');
-
+  const menu = await dbAll('select * from menu');
   res.json(menu);
 });
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,20 +5,20 @@ const promiseDecorator = require('../utils/promiseDecorator');
 const router = getRouter();
 const db = getDB();
 
-/* GET reviews listing. */
-router.get('/', async function(req, res, next) {
-  const dbAll = promiseDecorator(db.all.bind(db));
-  const reviews = await dbAll('SELECT * FROM reviews');
-
-  res.json(reviews);
-});
-
-/* GET reviews listing by id. */
+/* GET reviews listing */
 async function init() {
-  router.get('/get_by_product/:id', async function(req, res, next) {
-    const dbGet = promiseDecorator(db.all.bind(db));
-    const id = req.params.id;
-    const reviews = await dbGet(`SELECT * FROM reviews WHERE product_id=${id}`);
+  const dbGet = promiseDecorator(db.all.bind(db));
+
+  router.get('/load_by_ids/', async function(req, res, next) {
+    //console.log(req);
+    const reviews = await dbGet(`SELECT * FROM reviews WHERE product_id in (${req.query.ids.join(',')})`);
+    res.json(reviews);
+  });
+
+  router.get('/:start/:end', async function(req, res, next) {
+    const start = req.params.start;
+    const end = req.params.end;
+    const reviews = await dbGet(`SELECT * FROM reviews WHERE product_id BETWEEN ${start} AND ${end}`);
 
     res.json(reviews);
   });
