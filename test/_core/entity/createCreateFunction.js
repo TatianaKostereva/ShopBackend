bb77737df24ef37408462b24c05a0334f14df2d5
webma <webmaster@inster1100.com>
2020-09-29 21:42:44 +0300

    fix

--- test/_core/entity/createCreateFunction.js
+++ test/_core/entity/createCreateFunction.js
@@ -2,57 +2,76 @@ const BD = require('../../_core/BD');
 const allItems = require('../_utils/getAllItems');
 const allPosts = allItems('posts');
 
+const createAuthor = (id, {name, date}) => {
+  return {
+    id,
+    name,
+    date,
+  };
+}
+
+const checkPost = (postId) => {
+  const checkingPost = allPosts.find((post) => post.id === postId);
+  if (!checkingPost) {
+    throw Error('пост не найден');
+  }
+}
+
+const checkAuthor = (authorId) => {
+  const checkingPost = BD.store.authors.some((post) => post.id === authorId);
+  if (!checkingPost) {
+    throw Error('не найден автор');
+  }
+}
+
+const createPost = (id, {text, authorId}) => {
+  checkAuthor(authorId);
+
+  return {
+    id,
+    text,
+    author_id: authorId,
+  };
+}
+
+const createComment = (id, {text, rate, authorId, postId}) => {
+  checkAuthor(authorId);
+  checkPost(postId);
+
+  return {
+    id,
+    text,
+    rate: rate,
+    author_id: authorId,
+    post_id: postId,
+  };
+}
+
+const mapCreators = {
+  'posts': {
+    checks: [
+      checkAuthor,
+    ],
+    fieldsMap: {
+      id: 'id',
+      text: 'text',
+      author_id: 'authorId',
+    }
+  },
+  'authors': createAuthor,
+  'comments': createComment,
+}
+
 const createCreateFunction = (listName) => {
   const list = allItems(listName);
   const id = list[list.length - 1].id + 1;
 
-  return ({postId, authorId, text, rate, name, date}) => {
-
-    if (listName === 'posts' || listName === 'comments') {
-      if (!BD.store.authors.some((author) => author.id === authorId)) {
-        throw Error('не найден автор');
-      }
-    }
-
-    if (listName === 'authors') {
-      const newAuthor = {
-        id,
-        name,
-        date,
-      };
-
-      list.push(newAuthor);
-      return true;
-    }
-
-    if (listName === 'posts') {
-      const newPost = {
-        id,
-        text,
-        author_id: authorId,
-      };
-
-      list.push(newPost);
-      return true;
-    }
-
-    if (listName === 'comments') {
-      const checkingPost = allPosts.find((post) => post.id === postId);
-      if (!checkingPost) {
-        throw Error('пост не найден');
-      }
-
-      const newComment = {
-        id,
-        text,
-        rate: rate,
-        author_id: authorId,
-        post_id: postId,
-      };
-      list.push(newComment);
-      return true;
-    }
+  return (data) => {
+    const item = mapCreators[listName](id, data);
+    list.push(item);
+    return true;
   }
 }
+createCreateFunction('posts')({authorId: 17, text: 'text 3'})
 
 module.exports = createCreateFunction;
